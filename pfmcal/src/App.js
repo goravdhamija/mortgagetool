a0ac5ac05028d4dff30a97edb3c153a1bad30e37
Gaurav Dhamija <gorav.dhamija@gmail.com>
2024-09-08 00:36:25 +0530

    initial setup 3

--- pfmcal/src/App.js
+++ pfmcal/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import logo from './pacific-financial-logo.jpg';
 import './App.css';
 
@@ -9,22 +10,64 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <UserInputsComponent />
+        <UserInputsComponentHeader />
+        <UserInputsComponentForm />
       </header>
     </div>
   );
 }
 
-function UserInputsComponent() {
+function UserInputsComponentHeader() {
   const [personName,setPersonName] = React.useState('');
-
   return (
     <div className="UserInputs">
          
             <h1>Pacific Financial Mortgage Calculator {personName}</h1>
-            <input type="text" onChange={(e) => setPersonName(e.target.value)}/>
+            {/* <input type="text" onChange={(e) => setPersonName(e.target.value)}/> */}
     
     </div>
+  );
+}
+
+
+
+function UserInputsComponentForm() {
+  const [newCompany, setCompany] = React.useState("");
+  const [newPosition, setPosition] = React.useState("");
+  const [newLink, setNewLink] = React.useState("");
+  const [newDate, setNewDate] = React.useState("");
+  const [newNote, setNewNote] = React.useState("");
+
+  return (
+        <form>
+          <input
+            value={newCompany}
+            onChange={(e) => setCompany(e.target.value)}
+            label="Company"
+            placeholder="Company"
+          />
+          <input
+            value={newPosition}
+            onChange={(e) => setPosition(e.target.value)}
+            label="Job Title"
+          />
+          <input
+            value={newLink}
+            onChange={(e) => setNewLink(e.target.value)}
+            label="Job Link"
+          />
+          <input
+            type="date"
+            value={newDate}
+            onChange={(e) => setNewDate(e.target.value)}
+          />
+          <input
+            value={newNote}
+            onChange={(e) => setNewNote(e.target.value)}
+            label="Note"
+          />
+          <button type="submit"> Submit </button>
+        </form>
   );
 }
 
